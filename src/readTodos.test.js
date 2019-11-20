cf860f893278378dd663363b1b427058b1b96e22
Cristobal Heis <cristobal.heiss.j@gmail.com>
2019-11-20 16:51:02 +0100

    fixed readTodos test

--- src/readTodos.test.js
+++ src/readTodos.test.js
@@ -1,9 +1,10 @@
 const readTodos = require('./readTodos');
+const introText = "============================\nHERE ARE YOUR CURRENT TODOS\n============================\n";
 
 test("it should display one todo", ()=>{
-  expect(readTodos(["todo1"])).toBe("* todo1\n")
+  expect(readTodos(["todo1"])).toBe(introText + "* todo1\n")
 })
 
 test("it should display all todos", ()=>{
-  expect(readTodos(["todo1", "todo2", "todo3"])).toBe("* todo1\n* todo2\n* todo3\n")
+  expect(readTodos(["todo1", "todo2", "todo3"])).toBe(introText + "* todo1\n* todo2\n* todo3\n")
 })
