f5f530a4103e8c393de4ab94b7b158c1f23dc347
Cristobal Heis <cristobal.heiss.j@gmail.com>
2019-11-25 09:17:44 +0100

    trying to make it executable, not succesful yet

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//const readline = require('readline');
+#!/usr/bin/env node
 const readTodosFromFile = require("./fileOperations").readTodosFromFile;
 const addTodoInFile = require("./fileOperations").addTodoInFile;
 const deleteTodoFromFile = require("./fileOperations").deleteTodoFromFile;
@@ -8,31 +8,23 @@ const readTodos = require("./readTodos");
 const updateTodo = require("./updateTodo");
 const deleteTodo = require("./deleteTodo");
 
-/*
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('What do you want to do next? ', (answer) => {
-  if (answer === "read"){
-    readTodosFromFile(readTodos);
-  } else {
-    console.log("alrighty")
-  };
-  rl.close();
-});
-*/
 
 let command = process.argv[2];``
 let firstArg = process.argv[3];
 let secondArg = process.argv[4];
+
+const runHelper = () => {
+  console.log(
+    "to read a list of todos type --> read\nto edit a todo type -> edit position-of-todo 'new-value-for-todo'\nto delete a todo type -> delete position-of-todo\nto add a new todo type -> add 'new-todo-value'\nIMPORTANT, each time you enter a new value it should be in quotation marks");
+}
+
 
 let startFunc = (command, firstArg, secondArg) => {
   if (command === "add") addTodoInFile(createTodo, firstArg);
   if (command === "read") readTodosFromFile(readTodos);
   if (command === "edit") updateTodoInFile(updateTodo, firstArg, secondArg);
   if (command === "delete") deleteTodoFromFile(deleteTodo, firstArg);
+  if (command === "help") runHelper();
 }
 
 startFunc(command, firstArg, secondArg);
