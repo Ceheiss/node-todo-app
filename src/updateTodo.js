cb32f576790b4314ec5e9eb32a488a094a88f20d
Cristobal Heis <cristobal.heiss.j@gmail.com>
2019-11-25 15:02:53 +0100

    added some comments

--- src/updateTodo.js
+++ src/updateTodo.js
@@ -1,10 +1,13 @@
+// use of destructuring in the second parameter
+// added default value to prevent errors
 const updateTodo = (state, {position, newValue} = {}) => {
   if(typeof arguments[1].newValue !== "string"){
     Promise.reject("Please provide a string to edit the todo");
   }
+  // copy of original is to keep immutability
   let returnedArray = state.slice(0);
   returnedArray[position] = newValue;
   return Promise.resolve(returnedArray);
 }
-// destructuring. Default value prevents error if passed param is not an object
+
 module.exports = updateTodo;
