3a67b7beaef926bc91839a9aadc12bc9ad433e25
Cristobal Heis <cristobal.heiss.j@gmail.com>
2019-11-25 15:27:02 +0100

    fixed bug in update function (you could inject unexisting positions, creating multiple null todos

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,9 +1,6 @@
 const createTodo = (newTodo, state) => {
-  if (typeof newTodo !== "string"){
-    Promise.reject("Please enter a string");
-  }
-  if (newTodo === ""){
-    Promise.reject("Please enter a todo");
+  if (typeof newTodo !== "string" || newTodo === ""){
+    return Promise.reject("Please enter a todo");
   }
   let returnedArray = state.slice(0);
   returnedArray.push(newTodo);
--- src/updateTodo.js
+++ src/updateTodo.js
@@ -1,8 +1,11 @@
 // use of destructuring in the second parameter
 // added default value to prevent errors
 const updateTodo = (state, {position, newValue} = {}) => {
-  if(typeof arguments[1].newValue !== "string"){
-    Promise.reject("Please provide a string to edit the todo");
+  if(typeof newValue !== "string"){
+    return Promise.reject("Please provide a string to edit the todo");
+  }
+  if(position > state.length){
+    return Promise.reject(new Error("There is no todo in that position"));
   }
   // copy of original is to keep immutability
   let returnedArray = state.slice(0);
